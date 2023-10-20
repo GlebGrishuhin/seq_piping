4dc1d6ed15f47dae4daadcda4e1c20008d057e5a
Gleb Grishuhin <89195544065@bk.ru>
2023-10-20 21:28:49 +0300

    Выполнены задачи к 21.10.2023 - Расчеты разными методами и вывод разделены по функциям; Задействован буфер из pde_solvers

--- DensitySeqPart/DensitySeqPart.cpp
+++ DensitySeqPart/DensitySeqPart.cpp
@@ -7,8 +7,6 @@
 
 #include <fixed/fixed.h>
 #include <pde_solvers/pde_solvers.h>
-#include "DensitySeqPart.h"
-
 
 using namespace std;
 
@@ -42,87 +40,85 @@ struct ro_problem
 
 /// @brief Расчет следующего слоя по свойству самоподобия
 /// @param problem Ссылка на структуру ro_problem
-/// @param buffer Ссылка на буфер
-void step_self_sim(ro_problem& problem, custom_buffer_t<single_var_moc_t>& buffer)
+/// @param curr Ссылка на текущий слой в буфере
+/// @param prev Ссылка на предыдущий слой в буфере
+void step_self_sim(ro_problem& problem, single_var_moc_t& curr, single_var_moc_t& prev)
 {
-    buffer.advance(+1);
-    vector<double>& next = buffer.current().vars.point_double[0];
-    vector<double>& prev = buffer.previous().vars.point_double[0];
+    vector<double>& layer = curr.vars.point_double[0];
+    vector<double>& prev_layer = prev.vars.point_double[0];
     if (problem.Q > 0)
             {
                 transform(
-                    prev.begin(), // Текущая точка
-                    prev.end() - 1,   // Предыдущая точка
-                    next.begin() + 1,
+                    prev_layer.begin(), // Текущая точка
+                    prev_layer.end() - 1,   // Предыдущая точка
+                    layer.begin() + 1,
                     [](double a) { return a; }
                 );
-                next[0] = problem.ro_left; // Дополнение слоя начальным условием
+                layer[0] = problem.ro_left; // Дополнение слоя начальным условием
             }
             else
             {
                 transform(
-                    prev.begin() + 1, // Текущая точка
-                    prev.end(),   // Предыдущая точка
-                    next.begin(),
+                    prev_layer.begin() + 1, // Текущая точка
+                    prev_layer.end(),   // Предыдущая точка
+                    layer.begin(),
                     [](double a) { return a; }
                 );
-                next.pop_back();
-
-                next.push_back(problem.ro_right); // Дополнение слоя начальным условием
+                layer.pop_back();
+                layer.push_back(problem.ro_right); // Дополнение слоя начальным условием
             }        
 }
 
 
 /// @brief Расчет следующего слоя уголком
 /// @param problem Ссылка на структуру ro_problem
-/// @param buffer Ссылка на буфер
-void step_corner(ro_problem& problem, custom_buffer_t<single_var_moc_t>& buffer)
+/// @param curr Ссылка на текущий слой в буфере
+/// @param prev Ссылка на предыдущий слой в буфере
+void step_corner(ro_problem& problem, single_var_moc_t& curr, single_var_moc_t& prev)
 {
-    buffer.advance(+1);
-    vector<double>& next = buffer.current().vars.point_double[0];
-    vector<double>& prev = buffer.previous().vars.point_double[0];
+    vector<double>& layer = curr.vars.point_double[0];
+    vector<double>& prev_layer = prev.vars.point_double[0];
     
     double sigma = problem.dt / problem.xStep;
     if (problem.Q > 0)
     {
         transform(
-            prev.begin(), // Текущая точка
-            prev.end() - 1,   // Предыдущая точка
-            prev.begin() + 1,
-            next.begin() + 1,
+            prev_layer.begin(), // Текущая точка
+            prev_layer.end() - 1,   // Предыдущая точка
+            prev_layer.begin() + 1,
+            layer.begin() + 1,
             [&sigma](double a, double b) { return (sigma * a) + ((1 - sigma) * b); }
         );
-        next[0] = problem.ro_left; // Дополнение слоя начальным условием
+        layer[0] = problem.ro_left; // Дополнение слоя начальным условием
     }
     else
     {
         transform(
-            prev.begin() + 1, // Текущая точка
-            prev.end(),   // Предыдущая точка
-            prev.begin(),
-            next.begin(),
+            prev_layer.begin() + 1, // Текущая точка
+            prev_layer.end(),   // Предыдущая точка
+            prev_layer.begin(),
+            layer.begin(),
             [&sigma](double a, double b) { return (sigma * a) + ((1.0 - sigma) * b); }
         );
-        next.pop_back();
-        next.push_back(problem.ro_right); // Дополнение слоя начальным условием
+        layer.pop_back();
+        layer.push_back(problem.ro_right); // Дополнение слоя начальным условием
     }
 }
 
 
 /// @brief Вывод текущего слоя в консоль
 /// @param problem Ссылка на структуру ro_problem
-/// @param buffer Ссылка на буфер
+/// @param curr Ссылка на текущий слой в буфере
 /// @param step Шаг моделирования
-void layer_to_console(ro_problem& problem, custom_buffer_t<single_var_moc_t>& buffer, int step)
+void layer_to_console(ro_problem& problem, single_var_moc_t& curr, int step)
 {
-    vector<double>& next = buffer.current().vars.point_double[0];
-    vector<double>& prev = buffer.previous().vars.point_double[0];
-    for (size_t i = 0; i < next.size(); i++)
+    vector<double>& layer = curr.vars.point_double[0];
+    for (size_t i = 0; i < layer.size(); i++)
             printf("Шаг = %d    t = %.3F    x = %.3F    ro = %.3F\n",
                 step,
                 step * problem.dt,
                 i * problem.xStep,
-                next[i]);
+                layer[i]);
 }
 
 
@@ -130,7 +126,7 @@ void layer_to_console(ro_problem& problem, custom_buffer_t<single_var_moc_t>& bu
 /// @param problem Ссылка на структуру ro_problem
 void task_to_console(ro_problem& problem)
 {
-    cout << "=== Описание задачи === " << problem.quantity << endl;
+    cout << "===== Описание задачи ===== " << endl;
     cout << "Рассчитываемая величина = " << problem.quantity << endl;
     cout << "Длина трубы = " << problem.L << endl;
     cout << "Время моделирования = " << problem.maxT << endl;
@@ -139,28 +135,27 @@ void task_to_console(ro_problem& problem)
     cout << "Шаг во времени = " << problem.dt << endl;
     cout << "Число узлов по времени = " << (int)(problem.maxT / problem.dt) << endl;
     cout << "Число узлов по координате = " << (problem.L / problem.xStep) + 1 << endl;
-    cout << "======================= " << problem.quantity << endl;
+    cout << "=========================== " << endl;
 }
 
 
 /// @brief Запись текущего слоя в файл
 /// @param problem Ссылка на структуру ro_problem
-/// @param buffer Ссылка на буфер
+/// @param curr Ссылка на текущий слой в буфере
 /// @param step Шаг моделирования
 /// @param filename Имя файла
-void layer_to_file(ro_problem& problem, custom_buffer_t<single_var_moc_t>& buffer, int step, string filename = "data.txt")
+void layer_to_file(ro_problem& problem, single_var_moc_t& curr, int step, string filename = "data.txt")
 {
-    vector<double>& next = buffer.current().vars.point_double[0];
-    vector<double>& prev = buffer.previous().vars.point_double[0];
+    vector<double>& layer = curr.vars.point_double[0];
     std::ofstream out;
     if (step == 0)
         out.open(filename);
     else
         out.open(filename, ios::app);
     if (out.is_open())
-        for (size_t i = 0; i < next.size(); i++)
+        for (size_t i = 0; i < layer.size(); i++)
         {
-            out << step * problem.dt << ',' << i * problem.xStep << ',' << next[i] << endl;
+            out << step * problem.dt << ',' << i * problem.xStep << ',' << layer[i] << endl;
         }
     out.close();
 }
@@ -186,20 +181,24 @@ void calc_ro_problem(ro_problem& problem, custom_buffer_t<single_var_moc_t>& buf
             {
                 task_to_console(problem);
             }
-            layer_to_console(problem, buffer, step);
+            layer_to_console(problem, buffer.current(), step);
         }
-        layer_to_file(problem, buffer, step);
-        
+        single_var_moc_t curr = buffer.current();
+        single_var_moc_t prev = buffer.previous();
+
+        layer_to_file(problem, buffer.current(), step);
+        buffer.advance(1);
+
         switch (method)
         {
         case 0:
         {
-            step_self_sim(problem, buffer);
+            step_self_sim(problem, buffer.current(), buffer.previous());
             break;
         }
         case 1:
         {
-            step_corner(problem, buffer);
+            step_corner(problem, buffer.current(), buffer.previous());
             break;
         }
         }
@@ -228,7 +227,7 @@ int main()
     printf("Время расчета: %.2fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
 
     // Вызов скрипта построения графиков
-    system("py PlotPrecomputedGraph.py");
+    //system("py PlotPrecomputedGraph.py");
 
     return 0;
 }
